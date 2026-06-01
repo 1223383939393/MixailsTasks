bc099959ee8836325356e9600bb1dd14bc87cbd4
gegem0n <maratsafarov430@gmail.com>
2026-06-01 15:40:59 +0300

    4 задание сделать

---
+++ summerTask4/Employer.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+using namespace std;
+class Employer
+{
+	string name;
+	int age;
+public: 
+	virtual void Print() = 0;
+};
+
+class President
+{
+
+};
+
+class Manager
+{
+
+};
+
+class Worker
+{
+
+};
+
